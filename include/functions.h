80369a62cfc2f4c94839e4cbfbb42955fefed2c4
DylanJakeComilang <122947025+DylanJakeComilang@users.noreply.github.com>
2023-10-31 16:31:09 -1000

    f***
    yeah

---
+++ include/functions.h
@@ -0,0 +1,175 @@
+#include "vex.h"
+
+///////////////////////////
+//   TELE-OP FUNCTIONS   //
+///////////////////////////
+
+///////////////////
+// FLYWHEEL CODE //////////////////////////////////////////////////////////////////////
+///////////////////
+
+bool isFly = false;
+int flySpeed = 50;
+
+void fling(){
+if(Controller1.ButtonA.pressing()){ //toggle Flywheel
+      if(!isFly){
+        isFly = true;
+      }
+      else if(isFly){
+        isFly = false;
+      }
+    }
+
+    if(isFly){
+      if(Controller1.ButtonUp.pressing() && flySpeed < 100){ // increase Flywheel speed by 10 pct
+        flySpeed += 10;
+      }
+      else if(Controller1.ButtonDown.pressing() && flySpeed > 0){ // decrease Flywheel speed by 10 pct
+        flySpeed -= 10;
+      }
+      FlyWheel.spin(forward, flySpeed, pct);
+    }
+    else{
+      FlyWheel.stop();
+    }
+}
+/////////////////
+// INTAKE CODE ///////////////////////////////////////////////////////////////////////
+/////////////////
+
+bool iTake = false;
+bool oTake = false;
+
+void toggleIntake(){
+if(Controller1.ButtonR1.pressing()){ // toggle Outtake
+      if(!oTake){
+        oTake = true;
+        iTake = false;
+      }
+      else if(oTake){
+        oTake = false;
+      }
+    }
+
+    if(Controller1.ButtonR2.pressing()){ // toggle Intake
+      if(!iTake){
+        iTake = true;
+        oTake = false;
+      }
+      else if(iTake){
+        iTake = false;
+      }
+    }
+
+    if(iTake){
+      Intake.spin(forward);
+    }
+    else if(oTake){
+      Intake.spin(reverse);
+    }
+    else{
+      Intake.stop();
+    }
+}
+////////////////////
+// PNEUMATIC CODE /////////////////////////////////////////////////////////////////////
+////////////////////
+void toggleFlaps(){
+  if(!Flaps.value()){
+    Flaps.set(true);
+  }
+  else if(Flaps.value()){
+    Flaps.set(false);
+  }
+}
+
+void toggleRatchet(){
+  if(!Ratchet.value()){
+    Flaps.set(true);
+  }
+  else if(Ratchet.value()){
+    Flaps.set(false);
+  }
+}
+//////////////
+// ARM CODE ///////////////////////////////////////////////////////////////////////////
+//////////////
+
+void moveArm(){
+  if(Controller1.ButtonL1.pressing()){
+    Arm1.spin(forward);
+    Arm2.spin(forward);
+  } else if (Controller1.ButtonL2.pressing()){
+    Arm1.spin(reverse);
+    Arm2.spin(reverse);
+  }else{
+    Arm1.stop();
+    Arm2.stop();
+  }
+}
+
+//////////////////////
+//   MEASUREMENTS   //
+//////////////////////
+
+double getArmEnc() { return Arm1.position(rotationUnits::raw); }
+void resetArmEnc() { Arm1.setPosition(0, rotationUnits::raw); }
+
+void stats(){
+  Controller1.Screen.clearLine();
+  Controller1.Screen.setCursor(0, 1);
+  Controller1.Screen.print(flySpeed);
+  Controller1.Screen.setCursor(0, 2);
+  Controller1.Screen.print(Drivetrain.heading());
+  Controller1.Screen.setCursor(0, 3);
+  Controller1.Screen.print(Flaps.value());
+}
+
+//////////////////////////////
+//   AUTONOMOUS FUNCTIONS   //
+//////////////////////////////
+
+void setArm(double kp, double setpoint){
+  resetArmEnc();
+  double speed = 0;
+  if(getArmEnc() < setpoint){
+    while(getArmEnc() < setpoint){
+      speed = setpoint - getArmEnc();
+      speed *= kp;
+      Arm1.spin(forward, speed, pct);
+      Arm2.spin(forward, speed, pct);
+  }
+  } else{
+    while(getArmEnc() > setpoint){
+      speed = setpoint - getArmEnc();
+      speed *= kp;
+      Arm1.spin(forward, speed, pct);
+      Arm2.spin(forward, speed, pct);
+    }
+  }
+}
+
+void autoFling(double speed ,double tim3){
+  FlyWheel.spin(forward, speed, pct);
+  wait(tim3-3,sec);
+  Controller1.rumble(".--");
+  wait(3, sec);
+  FlyWheel.stop();
+}
+
+void intakingForward(double botSpeed, double dist){
+  Intake.spin(forward, 80, pct);
+  Drivetrain.setDriveVelocity(botSpeed, pct);
+  Drivetrain.driveFor(forward, dist, inches);
+  Intake.stop();
+}
+
+void intakingForTime(double botSpeed, double point){
+  Intake.spin(forward, 80, pct);
+  Drivetrain.setDriveVelocity(botSpeed, pct);
+  Drivetrain.drive(forward);
+  wait(point, sec);
+  Intake.stop();
+  Drivetrain.stop();
+}
---
+++ src/main.cpp
@@ -0,0 +1,108 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       VEX                                                       */
+/*    Created:      Thu Sep 26 2019                                           */
+/*    Description:  Competition Template                                      */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// Drivetrain           drivetrain    1, 2, 3, 4, 5   
+// FlyWheel             motor         10              
+// Intake               motor         7               
+// Arm2                 motor         8               
+// Arm1                 motor         9               
+// Flaps                digital_out   A               
+// Ratchet              digital_out   B               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+#include "functions.h"
+
+using namespace vex;
+
+// A global instance of competition
+competition Competition;
+
+// define your global instances of motors and other devices here
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton(void) {
+  vexcodeInit();
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void autonomous(void) {
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void usercontrol(void) {
+
+  while (1) {
+    stats(); // print values
+
+    Controller1.ButtonY.pressed(toggleFlaps); // toggle Flaps
+    Controller1.ButtonX.pressed(toggleRatchet); // toggle Ratchet
+
+    toggleIntake();
+    fling();
+    moveArm();
+
+    wait(20, msec); // Sleep the task for a short amount of time to
+                    // prevent wasted resources.
+  }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
+}
